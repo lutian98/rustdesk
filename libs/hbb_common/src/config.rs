17030efb0566ee49aea9849ce65e0724d7b1bba1
lutian98 <37563118+lutian98@users.noreply.github.com>
2024-09-17 17:05:45 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rudesk.lutian.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "6ExJhOG66SpxXN3b8pZsqPL7GAHZcoIPbe5K71iAtUg=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rudesk.shiho.one"];
+pub const PUBLIC_RS_PUB_KEY: &str = "+h2PmHQjbKFd5fxFZkuWUizVV3y2cCzE+yZ+UotNnMU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
